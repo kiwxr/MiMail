377e9705e319debc8f0c28f1277e98d41ae05e9f
unknown <1065016047@qq.com>
2020-12-05 14:20:13 +0800

    '12完成'

--- src/views/order/OrderConfirm.vue
+++ src/views/order/OrderConfirm.vue
@@ -6,20 +6,20 @@
           <div class="item-address">
             <h2 class="addr-title">收获地址</h2>
             <div class="addr-list clearfix">
-              <div class="addr-info" v-for="(item, index) in list" :key="index">
+              <div class="addr-info" :class="{'checked':addressCheck == index}" @click="addressCheck=index" v-for="(item, index) in list" :key="index">
                 <h2>{{item.receiverName}}</h2>
                 <div class="phone">{{item.receiverMobile}}</div>
                 <div class="street">{{item.receiverProvince+' '+item.receiverCity+' '+item.receiverDistrict+' '+item.receiverAddress}}</div>
                 <div class="action">
-                  <a href="javascript:;"class="fl">
+                  <a href="javascript:;"class="fl" @click="delAddress(item)">
                     <div class="icon icon-del"></div>
                   </a>
-                  <a href="javascript:;" class="fr">
+                  <a href="javascript:;" class="fr" @click="openAddressModel(item)">
                     <div class="icon icon-edit"></div>
                   </a>
                 </div>
               </div>
-              <div class="addr-add">
+              <div class="addr-add" @click="openAddressModel()">
                 <div class="icon-add"></div>
                 <div>添加新地址</div>
               </div>
@@ -74,26 +74,79 @@
             </div>
             <div class="btn-group">
               <a href="/#/cart" class="btn btn-default btn-large">返回购物车</a>
-              <a href="javascript:;" class="btn btn-large">去结算</a>
+              <a href="javascript:;" class="btn btn-large" @click="submitOrder">去结算</a>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <model title="删除确认"
+           btn-type="3"
+           :show-model="showDelModel"
+           @cancel="showDelModel=false"
+           @submit="submitAddress">
+      <template v-slot:body>
+        <p>确认删除该地址妈？</p>
+      </template>
+    </model>
+    <model title="添加收货地址"
+           btn-type="3"
+           :show-model="showEditModel"
+           @cancel="showEditModel=false"
+           @submit="submitAddress">
+      <template v-slot:body>
+       <div class="edit-wrap">
+         <div class="item">
+           <input type="text" class="input" placeholder="姓名" v-model="checkedItem.receiverName">
+           <input type="text" class="input" placeholder="手机号" v-model="checkedItem.receiverMobile">
+         </div>
+         <div class="item">
+           <select name="province" v-model="checkedItem.receiverProvince">
+             <option value="北京">北京</option>
+             <option value="河北">河北</option>
+           </select>
+           <select name="city" v-model="checkedItem.receiverCity">
+             <option value="北京">北京</option>
+             <option value="河北">河北</option>
+           </select>
+           <select name="district" v-model="checkedItem.receiverDistrict">
+             <option value="北京">北京</option>
+             <option value="河北">河北</option>
+           </select>
+         </div>
+         <div class="item">
+           <textarea name="street" v-model="checkedItem.receiverAddress"></textarea>
+         </div>
+         <div class="item">
+           <input type="text" class="input" placeholder="邮编" v-model="checkedItem.receiverZip">
+         </div>
+       </div>
+      </template>
+    </model>
   </div>
 </template>
 
 <script>
   import {request} from "../../api";
+  import {Message} from "element-ui";
+  import Model from "../../components/Model";
 
   export default {
     name: "OrderConfirm",
+    components: {Model},
     data(){
       return{
         list:[],
         cartList:[],
         cartTotalPrice: 0,
-        count:0
+        count:0,
+        checkedItem:{  //选中的商品对象
+
+        },
+        userAction: '-1',  //用户行为 0：新增 1：编辑 2：删除,
+        showDelModel: false,
+        showEditModel: false,
+        addressCheck: 0
       }
     },
     mounted() {
@@ -101,13 +154,7 @@
       this.getCartList()
     },
     methods:{
-      getAddressList(){
-        request({
-          url:'/shippings'
-        }).then(res => {
-          this.list = res.list
-        })
-      },
+      //获取购物车列表
       getCartList(){
         request({
           url: "/carts"
@@ -115,12 +162,104 @@
           let cartL = res.cartProductVoList || []
           this.cartTotalPrice = res.cartTotalPrice
           this.cartList = cartL.filter(item => item.productSelected)
-          console.log(this.cartTotalPrice)
           cartL.map(item => {
             this.count += item.quantity
           })
         })
+      },
+      //获取收货地址
+      getAddressList(){
+        request({
+          url:'/shippings'
+        }).then(res => {
+          this.list = res.list
+        })
+      },
+      //打开弹窗
+      openAddressModel(item){
+        this.checkedItem = item || {}
+        this.userAction = item?1:0
+        this.showEditModel = true
+      },
+      //删除地址
+      delAddress(item){
+        this.checkedItem = item
+        this.userAction = 2
+        this.showDelModel = true
+      },
+      //关闭弹窗
+      closeModel(){
+        this.checkedItem = {}
+        this.userAction = '-1'
+        this.showDelModel = false
+        this.showEditModel = false
+      },
+      //地址删除、编辑、新增 提交
+      submitAddress(){
+        let {checkedItem,userAction} = this
+        let method,url= '',data = {}
+        if(userAction == 0){
+          method = 'post'
+          url = '/shippings'
+        }else if(userAction == 1){
+          method = 'put'
+          url = `/shippings/${checkedItem.id}`
+        }else {
+          method = 'delete'
+          url = `/shippings/${checkedItem.id}`
+        }
+        if(userAction == 0 || userAction == 1){
+          let {receiverName, receiverMobile, receiverProvince, receiverCity, receiverDistrict, receiverAddress, receiverZip} = checkedItem
+          let errMsg = ''
+
+          if(!receiverName){errMsg = "请输入收货人名称"}
+          else if(!receiverMobile || !/\d{11}/.test(receiverMobile)){errMsg = "请输入正确格式的手机号"}
+          else if(!receiverProvince){errMsg = "请省份"}
+          else if (!receiverCity){errMsg = "请城市"}
+          else if(!receiverDistrict){errMsg = "请区县"}
+          else if(!receiverAddress){errMsg = "请输入详细地址"}
+          else if(!receiverZip || !/\d{6}/.test(receiverZip)){errMsg = "请输入正确的邮编"}
+          if(errMsg != ''){
+            Message.error(errMsg)
+            return
+          }
+          data={receiverName, receiverMobile, receiverProvince, receiverCity, receiverDistrict, receiverAddress, receiverZip}
+          console.log(receiverName)
+          console.log(data)
+        }
+        request({
+          url,
+          method,
+          data
+        }).then(res => {
+          this.closeModel()
+          this.getAddressList()
+          Message.success('操作成功')
+        })
+      },
+      //生产订单
+      submitOrder(){
+        let item = this.list[this.addressCheck]
+        if(!item){
+          Message.error('请选择收获地址')
+          return
+        }
+        request({
+          url:'/orders',
+          method: 'post',
+          data:{
+            shippingId:item.id
+          }
+        }).then(res => {
+          this.$router.push({
+            path:'/order/pay',
+            query:{
+              orderNo:res.orderNo
+            }
+          })
+        })
       }
+
     }
   }
 </script>
@@ -151,6 +290,7 @@
               height: 180px;
               border: 1px solid #e5e5e5;
               margin-right: 15px;
+              margin-bottom: 15px;
               padding: 15px 24px;
               font-size: 14px;
               color: #757575;
@@ -334,6 +474,40 @@
         }
       }
     }
+    .edit-wrap{
+      font-size: 14px;
+      color: #999999;
+      .item{
+        margin-bottom: 15px;
+        .input{
+          display: inline-block;
+          box-sizing: border-box;
+          width: 283px;
+          height: 40px;
+          line-height: 40px;
+          padding-left: 15px;
+          border: 1px solid #e5e5e5;
+          &+.input{
+            margin-left: 14px;
+          }
+        }
+        select{
+          box-sizing: border-box;
+          width: 100px;
+          height: 40px;
+          line-height: 40px;
+          border: 1px solid #e5e5e5;
+          margin-right: 15px;
+        }
+        textarea{
+          box-sizing: border-box;
+          height: 62px;
+          width: 100%;
+          padding: 13px 15px;
+          border: 1px solid #e5e5e5;
 
+        }
+      }
+    }
   }
 </style>
